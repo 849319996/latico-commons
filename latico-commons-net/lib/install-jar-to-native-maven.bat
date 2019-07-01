622513a649db8f52ae8eb907c17691b6e632de96
landingdong <landingdong@gdcattsoft.com>
2019-07-01 18:16:35 +0800

    增加jar包安装到本地

---
+++ latico-commons-net/lib/install-jar-to-native-maven.bat
@@ -0,0 +1,25 @@
+title jar install to native maven repository
+@echo off
+@rem 因为mvn命令也是脚本,在BAT文件里面执行Maven的mvn命令后，mvn之后的命令不会被执行，
+@rem 这是由于mvn本身也是BAT文件，并且其结束时执行了退出命令。要让mvn命令不使当前脚本自动退出，只需要在mvn之前加上“call”命令
+@rem 如果运行报mvn命令找不到，就配置好maven环境变量，如果还是找不到，请使用管理员方式运行脚本
+
+@rem 该脚本的使用，只需要修改jar的目录，必须使用绝对路径
+@echo on
+set jarAbsoluteDir=C:\project\latico\latico-commons\latico-commons-net\lib
+
+@rem ====================
+set jarFileName=AdventNetSnmp-4.0.4.jar
+set jarGroupId=com.adventnet.snmp
+set jarArtifactId=AdventNetSnmp
+set jarVersion=4.0.4
+call mvn install:install-file -Dfile=%jarAbsoluteDir%/%jarFileName% -DgroupId=%jarGroupId% -DartifactId=%jarArtifactId% -Dversion=%jarVersion% -Dpackaging=jar
+
+@rem ====================
+set jarFileName=AdventNetSnmp-4.0.4.jar
+set jarGroupId=com.adventnet.snmp
+set jarArtifactId=AdventNetLogging
+set jarVersion=4.0.4
+call mvn install:install-file -Dfile=%jarAbsoluteDir%/%jarFileName% -DgroupId=%jarGroupId% -DartifactId=%jarArtifactId% -Dversion=%jarVersion% -Dpackaging=jar
+
+pause
