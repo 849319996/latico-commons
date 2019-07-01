932ad610518abcce3132b3d45256c5883317d0ff
landingdong <landingdong@gdcattsoft.com>
2019-07-01 17:51:59 +0800

    增加jar包安装到本地

--- latico-commons-webservice-soap-wsdl/README.md
+++ latico-commons-webservice-soap-wsdl/README.md
@@ -1,1 +1,4 @@
+# 项目名称
+    wsdl操作工具，支持把http方式的wsdl地址读取生成XML请求模板和响应模板，也支持读取WSDL定义文件进行生成。
+    
 大部分Soap的jar包都是在SoapUI安装后的lib目录下，需要使用maven命令安装jar到本地仓库，或者上传到私服。
--- latico-commons-webservice-soap-wsdl/src/test/java/com/latico/commons/webservice/soap/wsdl/WsdlParerUtilsTest.java
+++ latico-commons-webservice-soap-wsdl/src/test/java/com/latico/commons/webservice/soap/wsdl/WsdlParerUtilsTest.java
@@ -20,13 +20,13 @@ public class WsdlParerUtilsTest {
 
     @Test
     public void readWsdlFile() {
-        System.out.println(WsdlParerUtils.readWsdlFile("E:\\公司文档\\凯通\\20160727DDPs\\DDPs\\ManageResourceInventory\\IIS\\wsdl\\EquipmentInventoryRetrieval\\EquipmentInventoryRetrievalMessages.wsdl", false));
+        System.out.println(WsdlParerUtils.readWsdlFile("./doc/ddps/ManageResourceInventory/IIS/wsdl/EquipmentInventoryRetrieval/EquipmentInventoryRetrievalHttp.wsdl", false));
 
     }
 
     @Test
     public void readWsdlFileDir() {
-        System.out.println(WsdlParerUtils.readWsdlFile("E:\\公司文档\\凯通\\20160727DDPs\\DDPs\\ManageResourceInventory\\IIS\\wsdl\\EquipmentInventoryRetrieval\\EquipmentInventoryRetrievalJms", false));
+        System.out.println(WsdlParerUtils.readWsdlFile("./doc/ddps/ManageResourceInventory/IIS/wsdl/EquipmentInventoryRetrieval/EquipmentInventoryRetrievalJms.wsdl", false));
 
     }
 }
