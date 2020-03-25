a50d41ae3e3201c7c3e3f0bedf6787aa5e957fd1
landingdong <landingdong@gdcattsoft.com>
2020-03-25 09:35:36 +0800

    增加hadoop操作工具

---
+++ latico-commons-hadoop/README.md
@@ -0,0 +1,2 @@
+# 注意
+    1、环境变量需要配置HADOOP_HOME，windows的是仿真类HADOOP_HOME配置，网上查找资料怎么配置。
---
+++ latico-commons-hadoop/src/test/java/com/latico/commons/hadoop/HdfsUtilsTest.java
@@ -0,0 +1,45 @@
+package com.latico.commons.hadoop;
+
+import org.apache.hadoop.fs.FileSystem;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HdfsUtilsTest {
+
+    /**
+     *
+     */
+    @Test
+    public void mkdir(){
+        FileSystem fileSystem = null;
+        try {
+            fileSystem = HdfsUtils.newInstanceFileSystem("hdfs://192.168.101.103:9000", "hadoop");
+            String dir = "/demo";
+            System.out.println("创建目录:" + HdfsUtils.mkdir(fileSystem, dir));
+            System.out.println("是否存在目录:" + HdfsUtils.existPath(fileSystem, dir));
+            System.out.println("删除目录:" + HdfsUtils.deleteFile(fileSystem, dir));
+            System.out.println("是否存在目录:" + HdfsUtils.existPath(fileSystem, dir));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            HdfsUtils.close(fileSystem);
+        }
+    }
+
+    @Test
+    public void test(){
+        FileSystem fileSystem = null;
+        try {
+            fileSystem = HdfsUtils.newInstanceFileSystem("hdfs://192.168.101.103:9000", "hadoop");
+            String dir = "/demo";
+            System.out.println(HdfsUtils.mkdir(fileSystem, dir));
+            System.out.println("是否存在目录:" + HdfsUtils.existPath(fileSystem, dir));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            HdfsUtils.close(fileSystem);
+        }
+    }
+
+}
